a15f9d45fe4a4db6405cd0faeabc14716d4d39f1
FerFlower <diegoquispeapaza45@gmail.com>
2025-03-01 06:42:23 -0500

    arreglo de nenu

--- resources/views/menu_management/new_menu_and_edit.blade.php
+++ resources/views/menu_management/new_menu_and_edit.blade.php
@@ -1,4 +1,4 @@
-,7k4                  <!--Encabezado de la pagina como plantilla de todo el panel de control-->
+<!--Encabezado de la pagina como plantilla de todo el panel de control-->
 @include($HeaderPanel)
 <!---------------------------------------------------------------------->
 <link rel="stylesheet" href="{{ asset($InventoryRegisterDesktop) }}">
